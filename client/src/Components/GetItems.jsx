bb3c827388515424fd486a4c5854cadf7712e8c8
Sharrar Khan <SharrarKhan728@gmail.com>
2020-02-23 17:40:58 -0500

    Working on mycloset

--- client/src/Components/GetItems.jsx
+++ client/src/Components/GetItems.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Header from "./Header";
 
 const GetItems = (props) => {
-    {console.log(props)}
+    {console.log("props:", props)}
     return(
         <div>
             {
-
+<h1>test</h1>
             }
         </div>
     );
--- client/src/Components/MyCloset.jsx
+++ client/src/Components/MyCloset.jsx
@@ -6,28 +6,54 @@ import GetItems from "./GetItems";
 class MyCloset extends React.Component {
     constructor() {
         super();
-        this.setState = {
+        this.state = {
             user: '1',
             data: []
         }
     }
-    
-    componentDidMount() {
-        this.getAllItems()
-    }
-    getAllItems = async () => {
-        try {
-            let clothes = await axios.get("http://localhost:3000/items");
+
+    async componentDidMount() {
+        // await this.getAllItems()
+    console.log(this.state.data)
+
+             try {
+            let clothes = await axios.get(`http://localhost:3000/items/`);
             console.log("clothes:", clothes);
+            console.log(clothes.data.payload)
+            let newData = [...this.state.data];
+            console.log("newData:", newData);
+            clothes.data.payload.map(element => {
+                newData.push(element)
+            });
             this.setState({
-                data: clothes.data.payload
+                data: newData
             });
 
         } catch (err) {
             console.log("ERROR:", err);
         }
     }
+    getAllItems = async () => {
+        // try {
+        //     let clothes = await axios.get(`http://localhost:3000/items/`);
+        //     console.log("clothes:", clothes);
+        //     console.log(clothes.data.payload)
+        //     let newData = [...this.state.data];
+        //     console.log("newData:", newData);
+        //     clothes.data.payload.map(element => {
+        //         newData.push(element)
+        //     });
+        //     this.setState({
+        //         data: newData
+        //     });
+
+        // } catch (err) {
+        //     console.log("ERROR:", err);
+        // }
+    }
+
     render() {
+        // const { user, data } = this.state;
         return (
             <div>
                 <Header />
@@ -35,7 +61,8 @@ class MyCloset extends React.Component {
                     <h2 className="componentHeaderTag">Clothing Checklist</h2>
                 </div>
                 <div id="myClosetContentDiv">
-                    <GetItems />
+                {this.state.data ? (<GetItems data={this.state.data} />) : null}
+                    
                 </div>
             </div>
         );
