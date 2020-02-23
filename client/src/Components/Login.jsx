6cd38227c978cb1dd542472eeff4ddc92bcd011f
Bpmauge <briahanamauge@pursuit.org>
2020-02-23 17:36:52 -0500

    created button for log in

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -6,28 +6,25 @@ import About from "./About";
 import Shops from "./Shops";
 
 const Login = () => {
-    return(
+    return (
         <div>
             <div className="componentHeaderDiv">
                 <h2 className="componentHeaderTag">Login</h2>
             </div>
-            <div id="myClosetContentDiv">
-                <p className="inputCaption">Username: </p>
-                <input className="inputField" type="text" placeholder=" Username" />
+            <form>
+                <div id="myClosetContentDiv">
+                    <p className="inputCaption">Username: </p>
+                    <input className="inputField" type="text" placeholder=" Username" />
 
-                <p className="inputCaption">Password: </p>
-                <input className="inputField" type="password" placeholder=" ***********" />
-            </div>
-            <div>
-                <h5></h5>
-            </div>
-            <div>
-                <nav id="HeaderNav">
-                    <Link to="/about" ><h3 className="HeaderNavElements">About</h3></Link>{" "}
-                    <Link to="/contact" ><h3 className="HeaderNavElements">Contact</h3></Link>{" "}
-                    <Link to="/shops" ><h3 className="HeaderNavElements">Stores</h3></Link>{" "}
-                </nav>  
-            </div>
+                    <p className="inputCaption">Password: </p>
+                    <input className="inputField" type="password" placeholder=" ***********" />
+                    <br></br>
+                <input type = 'button' value = 'Log In'/>
+                
+                </div>
+                
+                </form>
+         
         </div>
     );
 }
