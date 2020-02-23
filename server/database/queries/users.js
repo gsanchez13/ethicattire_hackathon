2b8e99d1e0cfa589253483697875f30fd6fc529c
Bpmauge <briahanamauge@pursuit.org>
2020-02-23 12:38:41 -0500

    user routes

---
+++ server/database/queries/users.js
@@ -0,0 +1,22 @@
+const db = require('../db')
+
+const addNewUser = async (user) => {
+	const newUserQuery = `
+		INSERT INTO users(username, password, style)
+			VALUES($/username/, $/password/, $/style/)
+			RETURNING id, username, style
+	`
+	const newUser = await db.one(newUserQuery, user)
+	return newUser;
+}
+
+const getUserById = async (username) => {
+    const user = await db.oneOrNone("SELECT * FROM users WHERE id = $1", [username])
+	return user;
+}
+
+module.exports = {
+	
+	addNewUser,
+	getUserById
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userQueries = require('../db/queries/users')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/user/:id', async (req, res) => {
+    try {
+        let user = req.params.user_id
+        let response = await userQueries.getUserById(user)
+        res.status(200).json({
+            payload: response,
+            msg: "Success getting user",
+            err: false
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            payload: null,
+            msg: "Failure retrieving user",
+            err: true
+        })
+    }
+})
+
+router.post('/signUp') = async (req, res) => {
+
+    try {
+        let userInfo = {
+            username: req.body.username,
+            password: req.body.password,
+            style: req.body.style
+        }
+        let newUser = await userQueries.createNewUser(userInfo);
+        res.status(200).json({
+            payload: null,
+            msg: "Success adding a new user",
+            err: false
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            payload: null,
+            msg: "Failure adding a new user",
+            err: true
+        })
+    }
+}
 
 module.exports = router;
