2c6e7e1e5c255fee1e77b5f80a03a9d8723145cc
Bpmauge <briahanamauge@pursuit.org>
2020-02-23 12:54:07 -0500

    removed try catch

--- server/database/queries/users.js
+++ server/database/queries/users.js
@@ -1,7 +1,6 @@
 const db = require('../pgExport')
 
 const addNewUser = async (user) => {
-    try {
         const newUserQuery = `
 		INSERT INTO users(username, password, style)
 			VALUES($/username/, $/password/, $/style/)
@@ -9,19 +8,13 @@ const addNewUser = async (user) => {
 	`
         const newUser = await db.one(newUserQuery, user)
         return newUser;
-    } catch (err) {
-        console.log(err)
-    }
+   
 }
 
 const getUserById = async (username) => {
-    try {
-
         const user = await db.oneOrNone("SELECT * FROM users WHERE id = $1", [username])
         return user;
-    } catch (err) {
-        console.log(err)
-    }
+    
 }
 
 module.exports = {
