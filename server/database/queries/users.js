1f2c2d1991d5ddb9c4c37c0558b3f810f2ef1bcd
Bpmauge <briahanamauge@pursuit.org>
2020-02-23 12:47:19 -0500

    added a try catch

--- server/database/queries/users.js
+++ server/database/queries/users.js
@@ -1,22 +1,31 @@
 const db = require('../pgExport')
 
 const addNewUser = async (user) => {
-	const newUserQuery = `
+    try {
+        const newUserQuery = `
 		INSERT INTO users(username, password, style)
 			VALUES($/username/, $/password/, $/style/)
 			RETURNING id, username, style
 	`
-	const newUser = await db.one(newUserQuery, user)
-	return newUser;
+        const newUser = await db.one(newUserQuery, user)
+        return newUser;
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 const getUserById = async (username) => {
-    const user = await db.oneOrNone("SELECT * FROM users WHERE id = $1", [username])
-	return user;
+    try {
+
+        const user = await db.oneOrNone("SELECT * FROM users WHERE id = $1", [username])
+        return user;
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 module.exports = {
-	
-	addNewUser,
-	getUserById
+
+    addNewUser,
+    getUserById
 }
