00e0e3429df05edf6348b713dd6aaa975859c0db
your name here <your_email_here>
2020-02-23 17:36:12 -0500

    adding fabrics to seed.sql

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const multer = require ('multer');
 
+
 const storage = multer.diskStorage({
    destination: (req, file, cb) => {
      cb(null, "./public/images")
@@ -28,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/items', itemsRouter);
 app.use('/users', usersRouter);
