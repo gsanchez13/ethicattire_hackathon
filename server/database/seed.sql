13631de7e59bb7c3edfff55e0960f38f9b58a6df
RougeLight <dantaeflowers@pursuit.org>
2020-02-23 16:22:57 -0500

    sweeping database changes, start of item component

--- server/database/seed.sql
+++ server/database/seed.sql
@@ -17,11 +17,16 @@ CREATE TABLE fabrics (
    score INT
 );
 
+CREATE TABLE clothes (
+   id SERIAL PRIMARY KEY,
+   clothes_type VARCHAR
+);
+
 CREATE TABLE items (
    id SERIAL PRIMARY KEY,
    item_img VARCHAR,
    fabric_id INT REFERENCES fabrics (id),
-   item_type VARCHAR,
+   clothes_id INT REFERENCES clothes (id),
    user_id INT REFERENCES users (id),
    color VARCHAR
 );
@@ -35,29 +40,30 @@ INSERT INTO fabrics (fabric_type, score)
             ('polyester', 0),
             ('linen', 0),
             ('silk', 1),
-            ('jersey', 0),
-            ('lycra', 0),
             ('spandex' , 0),
-            ('lace', 0),
-            ('crepe', 0),
-            ('velvet', 0),
             ('cotton-spandex', 0),
             ('polyester-cotton-rayon', 0),
             ('cotton-polyester', 0),
             ('polyester-spandex', 0);
 
-INSERT INTO items (item_img, fabric_id, item_type, user_id, color)
-   VALUES   ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',1, 'jeans', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',2, 'shirt', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',3, 'sweater', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',4, 'pants', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',5, 'shirt', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',6, 'shorts', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',7, 'cardigan', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',8, 'hat', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',9, 'jacket', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',10, 'sweats', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',11, 'jeans', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',12, 'hat', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',13, 'jeans', 1, 'blue'),
-            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',14, 'jeans', 1, 'blue');
+INSERT INTO clothes (clothes_type) 
+   VALUES   ('t-shirts'),
+            ('sweater'),
+            ('button down'),
+            ('jeans'),
+            ('trousers'),
+            ('blazer'),
+            ('coat'),
+            ('sneakers'),
+            ('dress shoes');
+
+INSERT INTO items (item_img, fabric_id, clothes_id, user_id, color)
+   VALUES   ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',1, 4, 1, 'blue'),
+            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',2, 1, 1, 'blue'),
+            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',3, 2, 1, 'blue'),
+            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',4, 5, 1, 'blue'),
+            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',5, 1, 1, 'blue'),
+            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',6, 5, 1, 'blue'),
+            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',7, 6, 1, 'blue'),
+            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',8, 9, 1, 'blue'),
+            ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',9, 7, 1, 'blue');
