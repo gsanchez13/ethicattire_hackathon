63d4115bf52e0b0cbda56cfb61d6b35bb5638376
SavitaMadray <52182983+SavitaMadray@users.noreply.github.com>
2020-02-23 13:22:12 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/items', itemsRouter);
 app.use('/users', usersRouter);
 app.use('/fabrics', fabricsRouter)
 
