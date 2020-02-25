2aeca9abd7219512b098972c5644c2485dfdf034
your name here <your_email_here>
2020-02-24 19:07:14 -0500

    added fabrics componeents

---
+++ client/src/Components/FabricItems/Silk.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+const Silk = () => {
+  return (
+    <div className="FabricTitle">
+      <h1>Silk Care</h1>
+      <img className="FabricImg" src="https://cdn.pixabay.com/photo/2018/09/11/20/43/silk-3670576_1280.jpg"></img>
+
+      <p className="FabricDesc">
+        Like our label says, when it comes to washing your silk leaving it to
+        the experts at the dry cleaners is the absolute best way to extend the
+        life of your silk and keep its subtle lustre and delicate hand feel
+        (especially if we’re talking stains). However, if you ever find yourself
+        in a jam or want an at home way to freshen your silk, then here is how
+        you can safely hand wash your favourite pieces.
+     
+      <h3>Hand Wash</h3>
+      <p>
+        You’ll want to make sure to do colours separately and read the detergent
+        label before using it on your silk. Words like “suitable for silk” and
+        “delicates” are your best friends when it comes to washing silk by hand.
+        Bleach will damage your silk’s fibres so it’s always a big no.
+      </p>
+      <h3>Steps</h3>
+      <ul>
+        <li>
+          Place your silk garment in cold water with a mild silk-friendly
+          detergent.
+        </li>
+        <li>Leave to soak (no more than 5 minutes).</li>
+        <li>Swish the garment slowly and gently.</li>
+        <li>Rinse with fresh water</li>
+        <li>
+          To help keep its hydrated feel, use a fabric conditioner in the final
+          rinse (or even a small amount of hair conditioner).
+        </li>
+        <li>Rinse well in cold water.</li>
+        <li>
+          Ball your silk together to remove excess moisture (wringing your silk
+          out will damage it’s fibres). Then lay it flat and roll it in a towel
+          to absorb any lingering moisture.
+        </li>
+        <li>Lay flat to dry.</li>
+      </ul>
+      <h3>Wrinkles and Creases</h3>
+      <p>
+        Most wrinkles in silk can simply be steamed out, but not everyone owns a
+        steamer. A great steamer hack is to hang your silk in the bathroom and
+        let it steam while you take a hot shower. If you can’t steam the creases
+        out then here are a few tips on how to safely iron your silk:
+      </p>
+      <ul>
+        <li>
+          Flip your garments inside out and set the iron to low heat (or the
+          silk setting). Iron silk only once it’s dry and be sure to put a cloth
+          between the silk and the iron. Do not spray or wet silk while ironing,
+          you may get water-stains.
+        </li>
+      </ul>
+      </p>
+    </div>
+  );
+};
+
+export default Silk;
--- server/database/seed.sql
+++ server/database/seed.sql
@@ -14,16 +14,14 @@ CREATE TABLE users (
 CREATE TABLE fabrics (
    id SERIAL PRIMARY KEY,
    fabric_type VARCHAR,
-<<<<<<< HEAD
-   fabric_desc VARCHAR
-=======
-   score INT
+   score INT,
+   fabric_desc VARCHAR,
+   fabric_img VARCHAR
 );
 
 CREATE TABLE clothes (
    id SERIAL PRIMARY KEY,
    clothes_type VARCHAR
->>>>>>> e44383d0cde498618f02eb72545c05ead7a03c5c
 );
 
 CREATE TABLE items (
@@ -39,27 +37,18 @@ CREATE TABLE items (
 INSERT INTO users (username, password, style) 
    VALUES ('sharr', 'east', false);
 
-<<<<<<< HEAD
-INSERT INTO fabrics (fabric_type, fabric_desc) 
-   VALUES   ('cotton','Cotton is one of the most commonly used fabrics. This 
-                     natural fibre is light and breathable but 
-                     growing cotton can be problematic: conventional cotton is one of the thirstiest 
-                     and most chemical-intensive crops to grow. It requires a lot of pesticides and, 
-                     as a result, has a negative impact on the planet, and the people who grow it. 
-                     Organic cotton, a more sustainable alternative to conventional cotton. It aims to lower the environmental 
-                     impact by trying to remove harmful pesticides and other chemicals from the production process. 
-                     If you’re looking for the most sustainable cotton, recycled cotton is the way to go. Recycled or 
-                     upcycled cotton is made using post-industrial and post-consumer cotton waste. 
-                     Recycled cotton has the potential to help reduce water and energy consumption, as well as help keep cotton 
-                     clothes out of landfill.'),
-            ('hemp','Hemp is gaining popularity. Marijuana’s ‘sober cousin’ is extremely 
-                        versatile: it’s used as a food, a building material, in cosmetics, and is one of the oldest fibres in the world,
-                        used for hundreds of years as a fabric. The great thing about hemp is that it’s grown all around the
-                        world and it requires very little water, no pesticides, and naturally fertilises the soil it grows 
-                        in – making it much better for the environment than other crops.
-                        Hemp helps keep you warm in winter and cool in summer, and gets softer the more you wash it. For all these reasons, we also consider hemp one of the most 
-                        sustainable fabrics out there.'),
-            ('polyester','Polyester is a man-made fiber that is synthesized from petroleum-based products. Polyester cloth was invented in 1941, so 
+INSERT INTO fabrics (fabric_type, score, fabric_desc, fabric_img) 
+   VALUES   ('Cotton', 1, 'Cotton is one of the most commonly used fabrics. This natural fibre is light and breathable but 
+                           growing cotton can be problematic: conventional cotton is one of the thirstiest and most 
+                           chemical-intensive crops to grow. It requires a lot of pesticides and, as a result, has a negative 
+                           impact on the planet, and the people who grow it. 
+                           Organic cotton, a more sustainable alternative to conventional cotton. It aims to lower the environmental 
+                           impact by trying to remove harmful pesticides and other chemicals from the production process. 
+                           If you’re looking for the most sustainable cotton, recycled cotton is the way to go. Recycled or 
+                           upcycled cotton is made using post-industrial and post-consumer cotton waste. 
+                           Recycled cotton has the potential to help reduce water and energy consumption, as well as help keep cotton 
+                           clothes out of landfill.', 'https://cdn.pixabay.com/photo/2019/11/24/17/08/cotton-4649804_1280.jpg'),
+            ('Polyester', 0,'Polyester is a man-made fiber that is synthesized from petroleum-based products. Polyester cloth was invented in 1941, so 
                         comparing to cotton, this fabric is a fairly new player in the game. The majority of polyesters are not biodegradable meaning 
                         that the polyester fabric shirt you bought last season will not decompose for 20 years at best and 200 years at worst, depending 
                         on conditions.
@@ -72,13 +61,13 @@ INSERT INTO fabrics (fabric_type, fabric_desc)
                         As polyester is made from plastic, there is the question as to whether it is bad for the environment. 
                         True, it does come from an unsustainable source, however, plastic bottles are actually recycled into 
                         polyester fabric, which is pretty amazing, but, on the downside, plastic is not compostable, which means 
-                        it doesn’t break down well in soil.'),
-            ('linen', 'Linen is another natural fibre we’ve been growing for centuries. Similar to hemp, it’s derived from a 
+                        it doesn’t break down well in soil.', 'https://cdn.pixabay.com/photo/2015/09/08/15/43/scarf-930185_1280.jpg'),
+            ('Linen', 0,'Linen is another natural fibre we’ve been growing for centuries. Similar to hemp, it’s derived from a 
                         very versatile crop: the flax plant. Linen requires minimal water and pesticides, and grows in 
                         poor-quality soil. Plus, every part of the plant is used, so nothing is wasted. Linen is strong, naturally 
                         moth resistant, and, when untreated (i.e. not dyed), fully biodegradable. In addition to being good for the 
-                        planet, it is also light and can withstand high temperatures, absorbing moisture without holding bacteria.'),
-            ('silk', 'Silk is one of the most luxurious types of fabric used in the fashion industry and also in products for the home. 
+                        planet, it is also light and can withstand high temperatures, absorbing moisture without holding bacteria.', 'https://cdn.pixabay.com/photo/2017/08/07/20/10/woven-2607340_1280.jpg'),
+            ('Silk', 1, 'Silk is one of the most luxurious types of fabric used in the fashion industry and also in products for the home. 
                       You can find it in cushions, pillow and duvet covers, bedspreads, lampshades and others. According to a Chinese 
                       legend, it was discovered by an empress who was sipping her tea under a mulberry tree when a cocoon fell into her 
                       cup and began to open up to reveal the shiny fibres. There is no doubt that silk is a beautiful material. It 
@@ -93,32 +82,61 @@ INSERT INTO fabrics (fabric_type, fabric_desc)
                       extraction of the silk thread from the cocoon.As with any other type of fabric silk is usually dyed. Many dyes applied to fabrics 
                       can be toxic and sometimes formaldehyde can be used in the finishing process of silk, which could affect your health. To avoid 
                       having any toxic materials in your home look for undyed silk or silk dyed with non-toxic dyes. Also, have a look for silk that is finished 
-                      with citric acid rather than formaldehyde.'),
-            ('jersey'),
-            ('lycra'),
-            ('spandex'),
-            ('lace'),
-            ('crepe'),
-            ('velvet'),
-            ('cotton-spandex'),
-            ('polyester-cotton-rayon'),
-            ('cotton-polyester'),
-            ('polyester-spandex');
-
-INSERT INTO items (fabric_id, item_img, item_type, user_id, color)
-   VALUES   (11,'https://pngimg.com/uploads/jeans/jeans_PNG5745.png', 'jeans', 1, 'blue');
-            
-=======
-INSERT INTO fabrics (fabric_type, score) 
-   VALUES   ('cotton', 1),
-            ('polyester', 0),
-            ('linen', 0),
-            ('silk', 1),
-            ('spandex' , 0),
-            ('cotton-spandex', 0),
-            ('polyester-cotton-rayon', 0),
-            ('cotton-polyester', 0),
-            ('polyester-spandex', 0);
+                      with citric acid rather than formaldehyde.', 'https://cdn.pixabay.com/photo/2012/07/09/07/19/silk-52003_1280.jpg'),
+            ('Spandex' , 0, 'Spandex, Lycra or elastane is a synthetic fiber known for its exceptional elasticity. It is stronger
+                             and more durable than natural rubber.  Spandex  is another polymer fabric (like polyester) but with 
+                             totally different properties.  Pieces of the polyurethane chain allow for stretching, hence the 
+                             rubber-like quality.
+                             Because of its elasticity and strength (stretching up to five times its length), spandex has been 
+                             incorporated into a wide range of garments, especially in skin-tight garments.
+                             Also like polyester, the process of making spandex takes raw materials, toxic chemicals, and a 
+                             lot of energy.  Spandex doesn’t have a super long life, which means you have to keep buying new 
+                             tights and leggings, bikinis and banana hammocks.
+                             Created from polyurethane, a known carcinogen
+                             Blends well with other fabrics
+                             Creates form fitting and highly durable garments
+                             Can be a cause of contact dermatitis
+                             Non-biodegradeable', ''),
+            ('Cashmere', 0, 'Cashmere doesn’t come from sheep, but from goats. The breed of goat is found 
+                             between Mongolia, Southwest China, Iran, Tibet, Northern India and Afghanistan.
+                             These goats have very little fat to protect them in the winter from the cold arid plains, so they 
+                             develop soft, fleecy fibres underneath their coat, on the underbelly. These  hairs are what makes 
+                             cashmere.
+                             When the temperatures rise, the goats naturally shed their coats. That’s when producers comb out 
+                             the fine hair, sort it by hand, send it to facilities to be cleaned, refined, baled and shipped to 
+                             Europe, where they’re sold to companies.
+                             Because of the rarity of the material (it takes four goats to produce enough hair to make one 
+                             sweater), cashmere was initially expensive. 
+                             These days, with the advent of fast fashion, it’s become easy to find cashmere.  But as always with 
+                             fast fashion, when you get a low price, someone or something is paying, somewhere.
+                             Goats are the first to pay the price of cheap cashmere production. As they have very little fat, 
+                             shearing them too early mid-winter means they can freeze to death.
+                             The grasslands of the Asian regions where the goats roam are also suffering. The increase in 
+                             demand for cashmere came with the pressure to lower cost, and so a decrease in the price of the raw 
+                             material. Herders now need more goats to produce the same amount of cashmere. More goats mean more 
+                             mouths to feed and overpopulation is killing these lands: once green and unspoilt, these regions 
+                             are quickly becoming deserts. This does not only affect these specific regions but creates an 
+                             ecological imbalance for the planet.', 'https://cdn.pixabay.com/photo/2019/07/28/08/20/billy-goat-4368163_1280.jpg'),
+            ('Cotton-spandex', 0, 'The fabric is nearly all cotton, it will have all the comfort and breathability of cotton.  
+                                   The spandex (LYCRA®) will not noticeably affect this since typically the blends only use 2% 
+                                   to 5% spandex, which acts to control the shape of the fabric so it doesn’t wrinkle or stretch.', 'https://cdn.pixabay.com/photo/2015/09/05/21/57/girl-925635_1280.jpg'),
+            ('Polyester-cotton-rayon', 0, '', ''),
+            ('Cotton-Polyester', 0, 'A fabric made from a poly cotton blend combines the strengths of the two fibers. Poly 
+                                     cotton garments are breathable, tear-resistant, and can be fashioned into abrasion-resistant
+                                     fabrics, like canvas. While not as inexpensive as pure polyester, poly cotton blends do 
+                                     tend to cost less than comparable garments made of 100% cotton and they provide much more 
+                                     comfort. The previously mentioned 65/35 blend of cotton and polyester is the most popular 
+                                     for work garments, particularly because of price, durability and a larger color selection 
+                                     being available for purchase. When shopping for work garments, the choice between 100% 
+                                     cotton and poly cotton blends depends largely on the work environment and personal 
+                                     preference. If employees work indoors around machinery, the tear-resistance of poly cotton 
+                                     may be favorable. If the work is in an outdoor setting, particularly in hot climates, 100% 
+                                     cotton fabric could be the better choice. Before deciding, make sure to take all 
+                                     environmental factors into consideration.', ''),
+            ('Polyester-pandex', 0, 'Spandex, also known as elastane or lycra, is a synthetic fiber best known for its elasticity. 
+                                      When combined with cotton, spandex makes a perfect fabric for clothing that is soft to touch and 
+                                      durable. Most commonly, polyester-spandex blends are used in activewear as spandex is stretchy and 
+                                      can retain its original shape, and polyester – durable.', '');
 
 INSERT INTO clothes (clothes_type) 
    VALUES   ('t-shirts'),
@@ -141,4 +159,3 @@ INSERT INTO items (item_img, fabric_id, clothes_id, user_id, color)
             ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',7, 6, 1, 'blue'),
             ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',8, 9, 1, 'blue'),
             ('https://pngimg.com/uploads/jeans/jeans_PNG5745.png',9, 7, 1, 'blue');
->>>>>>> e44383d0cde498618f02eb72545c05ead7a03c5c
